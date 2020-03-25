879d9a3dad5d514dd3359753840f45639a582bc1
Max Herman <maxherman@Maxs-MBP.lan>
2020-03-25 12:08:42 -0400

    Updates

---
+++ precache-manifest.bb79a16f02eab9f29d71ad32991273ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1cfe9840206deb75c22d758383fba56",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e1034c6d22dd0c15465a",
+    "url": "/monsters-rolodex/static/css/main.36bb439d.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1034c6d22dd0c15465a",
+    "url": "/monsters-rolodex/static/js/main.f4177318.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
